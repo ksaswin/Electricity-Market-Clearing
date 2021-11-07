627e2c4c11c8c3937031a6d513a6cf2a8ef27d36
Aswin <aswin2000.ak@gmail.com>
2021-11-07 17:17:44 +0530

    Moved headers into the function as a default argument

--- show_all_points.py
+++ show_all_points.py
@@ -1,10 +1,9 @@
 from tabulate import tabulate
 
-HEADERS = ['x-coordinate', 'y-coordinate']
 
-def show_marked_points(x, y):
+def show_marked_points(x, y, header_values = ['x-coordinate', 'y-coordinate']):
     x_and_y = []
     for index, x_value in enumerate(x):
         x_and_y.append([x_value, y[index]])
 
-    print(tabulate([*x_and_y], headers=HEADERS, tablefmt='orgtbl'))
+    print(tabulate([*x_and_y], headers=header_values, tablefmt='orgtbl'))
